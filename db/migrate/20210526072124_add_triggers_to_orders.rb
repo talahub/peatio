01038e07440d5a209d396ac9cf074fdbdc056750
denisfd <fedorchenko999@gmail.com>
2021-05-18 13:50:19 +0300

    Rename triggered_order_id to linked_order_id

--- db/migrate/20210526072124_add_triggers_to_orders.rb
+++ db/migrate/20210526072124_add_triggers_to_orders.rb
@@ -1,16 +1,14 @@
 class AddTriggersToOrders < ActiveRecord::Migration[5.2]
   def up
     drop_table :triggers
-    add_column :orders, :amount_quote, :decimal, precision: 32, scale: 16, after: :market_type
-    add_column :orders, :trigger_price, :decimal, precision: 32, scale: 16, after: :amount_quote
-    add_column :orders, :triggered_order_id, :decimal, precision: 32, scale: 16, after: :trigger_price
-    add_column :orders, :triggered_at, :datetime, after: :triggered_order_id
+    add_column :orders, :trigger_price, :decimal, precision: 32, scale: 16, after: :market_type
+    add_column :orders, :linked_order_id, :decimal, precision: 32, scale: 16, after: :trigger_price
+    add_column :orders, :triggered_at, :datetime, after: :linked_order_id
   end
 
   def down
-    remove_column :orders, :amount_quote
     remove_column :orders, :trigger_price
-    remove_column :orders, :triggered_order_id
+    remove_column :orders, :lenked_order_id
     remove_column :orders, :triggered_at
   end
 end
